c74d90e5791004396a5baf593860ba4b38263054
nadiinlove22-star <nadiinlove22@gmail.com>
2026-09-23 06:04:04 +0700

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,13 +1,18 @@
-// PWA Manifest Configuration
+// Konfigurasi Supabase API
+const SUPABASE_URL = 'https://wkjxohnhhgenfvxiorkh.supabase.co';
+const SUPABASE_ANON_KEY = 'EyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndranhvaG5oaGdlbmZ2eGlvcmtoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3OTAxMDI4MTgsImV4cCI6MjEwNTY3ODgxOH0.j5oNNjLnnmM0e-ltWftL09R9rMKaIQ6dyliE_aytTc8';
+
+// Inisialisasi Klien Supabase
+const supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+
+// Config Hari & PWA
 const manifestData = {
     "name": "DayliDo - Daily Todo List",
     "short_name": "DayliDo",
     "start_url": ".",
     "display": "standalone",
     "background_color": "#0b1329",
-    "theme_color": "#0b1329",
-    "description": "Aplikasi Todo List Harian Produktivitas",
-    "icons": [{ "src": "https://img.icons8.com/color/512/todo-list.png", "sizes": "512x512", "type": "image/png", "purpose": "any maskable" }]
+    "theme_color": "#0b1329"
 };
 const manifestString = JSON.stringify(manifestData);
 const manifestBlob = new Blob([manifestString], {type: 'application/json'});
